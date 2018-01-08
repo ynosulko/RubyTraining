919cfb70870f956a6c55e35d41d9d81518066767
Yevhen Nosulko <yevhen@Yevhens-MacBook-Pro-2.local>
2018-01-08 23:22:50 +0000

    Added exercise description

--- ex1.rb
+++ ex1.rb
@@ -1,3 +1,6 @@
+# Write a program that prompts for user input twice and comletes
+# the verse with entered user data
+
 puts 'Welcome to the jungle, we have ___ and ___'
 
 puts 'What should be the first word?'
